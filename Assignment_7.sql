2dd1654a59b4f7f1fe8b46e852949c49d7a039dd
Scott Mac McEachern <the_fixer@hotmail.com>
2019-03-19 20:15:24 -0700

    Completed questionn 3a

--- Assignment_7.sql
+++ Assignment_7.sql
@@ -74,4 +74,17 @@ WHERE
 	c.country IN ('Afghanistan', 'Bangladesh', 'China');
 
 
+/* 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, 
+so create a column in the table actor named description and use the data type BLOB 
+(Make sure to research the type BLOB, as the difference between it and VARCHAR are significant). */
+
+ALTER TABLE actor 
+	ADD COLUMN Description Blob;
+
+
+
+
+
+
+
 
