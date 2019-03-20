909b52dad81c2d3fad476ad61ffe9c68082aa264
Scott Mac McEachern <the_fixer@hotmail.com>
2019-03-19 19:51:44 -0700

    Completed question 1

--- Assignment_7.sql
+++ Assignment_7.sql
@@ -5,5 +5,22 @@ March 19, 2019
 Scott McEachern
 */
 
+-- Set current database
+use sakila;
 
--- 1a
+
+-- 1a. Display the first and last names of all actors from the table actor
+SELECT
+	a.first_name AS FirstName,
+    a.last_name AS LastName
+FROM 
+	actor a;
+
+
+-- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
+SELECT
+	UPPER(CONCAT(a.first_name, ' ', a.last_name)) AS `Actor Name`
+FROM 
+	actor a;
+
+
